0401bf43b0aca74eec8e968274577b4bf57e4235
peacemakerbak <peacemakerbak@gmail.com>
2024-05-24 12:19:10 -0400

    save text 6 results

--- frontend/components/Auth.test.js
+++ frontend/components/Auth.test.js
@@ -35,36 +35,46 @@ describe('Auth component', () => {
 
   // 👇 START WORKING HERE
   test('[1] Inputs acquire the correct values when typed on', async () => {
-    screen.debug()
     // ✨ type some text in the username input (done for you)
     await user.type(userInput, 'gabe')
     // ✨ assert that the input has the value entered (done for you)
     expect(userInput).toHaveValue('gabe')
     // ✨ type some text in the password input
+    await user.type(passInput, 'testpassword')
     // ✨ assert that the input has the value entered
-    expect(true).toBe(false) // DELETE
+    expect(passInput).toHaveValue('testpassword') 
   })
   test('[2] Submitting form clicking button shows "Please wait..." message', async () => {
     // ✨ type whatever values on username and password inputs
+    await user.type(userInput, 'gabe')
+    await user.type(passInput, 'testpassword')
     // ✨ click the Login button
+    await user.click(loginBtn)
     // ✨ assert that the "Please wait..." message is visible in the DOM
-    expect(true).toBe(false) // DELETE
+    expect(screen.getByText('Please wait...')).toBeVisible()
   })
   test('[3] Submitting form typing [ENTER] shows "Please wait..." message', async () => {
     // ✨ type whatever values in username and password inputs
+    await user.type(userInput, 'gabe')
+    await user.type(passInput, 'testpassword')
     // ✨ hit the [ENTER] key on the keyboard
+    await user.keyboard('[Enter]')
     // ✨ assert that the "Please wait..." message is visible in the DOM
-    expect(true).toBe(false) // DELETE
+    expect(screen.getByText('Please wait...')).toBeVisible()
   })
   test('[4] Submitting an empty form shows "Invalid Credentials" message', async () => {
     // ✨ submit an empty form
+    await user.click(loginBtn)
     // ✨ assert that the "Invalid Credentials" message eventually is visible
-    expect(true).toBe(false) // DELETE
+    expect(await screen.findByTestId('invalidCredentials')).toBeVisible()
   })
   test('[5] Submitting incorrect credentials shows "Invalid Credentials" message', async () => {
     // ✨ type whatever username and password and submit form
     // ✨ assert that the "Invalid Credentials" message eventually is visible
-    expect(true).toBe(false) // DELETE
+    await user.type(userInput, 'gabe')
+    await user.type(passInput, 'testpassword')
+    await user.click(loginBtn)
+    expect(await screen.findByTestId('Invalid Credentials')).toBeVisible()
   })
   for (const usr of registeredUsers) {
     test(`[6.${usr.id}] Logging in ${usr.username} makes the following elements render:
@@ -72,12 +82,18 @@ describe('Auth component', () => {
         - correct user info (ID, username, birth date)
         - logout button`, async () => {
       // ✨ type valid credentials and submit form
-      // ✨ assert that the correct welcome message is eventually visible
-      // ✨ assert that the correct user info appears is eventually visible
-      // ✨ assert that the logout button appears
-      expect(true).toBe(false) // DELETE
+      await user.type(userInput, usr.username)
+      await user.type(passInput, usr.password)
+      await user.click(loginBtn)
+      await waitFor(() => {
+        expect(screen.getByText(`Welcome back, ${usr.username}. We LOVE you!`)).toBeVisible()
+        expect(screen.getByText(`ID: ${usr.id} Username: ${usr.username} Born: ${usr.born}`)).toBeVisible()
+        expect(screen.getByText(`Logout`)).toBeVisible()
+      })
+      screen.debug()
     })
-  }
+
+      }
   test('[7] Logging out a logged-in user displays goodbye message and renders form', async () => {
     // ✨ type valid credentials and submit
     // ✨ await the welcome message
